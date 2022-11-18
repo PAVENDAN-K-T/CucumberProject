eac6119759e1bfa0c9d4126843981d4406a1764f
PAVENDAN-K-T <vendankt@gmail.com>
2022-11-18 09:09:19 +0530

    Ezhilan done his coding

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -1,9 +1,17 @@
 
 public class ForLoop {
-public static void main(String[] args) {
-	int a=50;
-	for (int i = 1; i <= a; i++) {
-		System.out.println(i);
+	public static void main(String[] args) {
+		// Master coding
+		int a = 50;
+		for (int i = 1; i <= a; i++) {
+			System.out.print(" " + i);
+		}
+		// Ezhilan coding
+		System.out.println();
+		int b = 50;
+		for (int j = b; j >= 1; j--) {
+			System.out.println(j);
+		}
+
 	}
-}
 }
